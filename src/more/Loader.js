85a1ce5d54c7b781ce974ec05bf0a579440408d7
Samip Subedi <samipsubedi68@gmail.com>
2025-03-02 12:56:54 +0545

    loader js

---
+++ src/more/Loader.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f4f4f4",
+      }}
+    >
+      <div
+        style={{
+          width: "50px",
+          height: "50px",
+          borderRadius: "50%",
+          border: "5px solid rgba(0, 0, 0, 0.1)",
+          borderTopColor: "#3498db",
+          animation: "spin 1s linear infinite",
+        }}
+      ></div>
+
+      <style>
+        {`
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default Loading;
