644cd1e20d6a74aed962c028da35a89ba6a41e1c
Samip Subedi <samipsubedi68@gmail.com>
2025-03-02 12:09:46 +0545

    uploading

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -37,3 +37,55 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
 } from "../constants/UserConstants";
+// Login
+export const login = (email, password) => async (dispatch) => {
+    try {
+      dispatch({ type: LOGIN_REQUEST });
+  
+      const config = { headers: { "Content-Type": "application/json" } };
+  
+      const { data } = await axios.post(
+        `/api/v2/login`,
+        { email, password },
+        config
+      );
+      dispatch({ type: LOGIN_SUCCESS, payload: data.user });
+    } catch (error) {
+      dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
+    }
+  };
+  
+  // Register
+  export const register = (userData) => async (dispatch) => {
+    try {
+      dispatch({ type: REGISTER_USER_REQUEST });
+  
+      const config = { headers: { "Content-Type": "multipart/form-data" } };
+  
+      const { data } = await axios.post(`/api/v2/register`, userData, config);
+  
+      dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
+      // relaod the page
+     
+    } catch (error) {
+      dispatch({
+        type: REGISTER_USER_FAIL,
+        payload: error.response.data.message,
+      });
+    }
+  };
+  // Load User
+export const loadUser = () => async (dispatch) => {
+    try {
+      dispatch({ type: LOAD_USER_REQUEST });
+      // eslint-disable-next-line
+      const config = { headers: { "Content-Type": "application/json" } };
+  
+      const { data } = await axios.get(`/api/v2/me`);
+  
+      dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
+    } catch (error) {
+      dispatch({ type: LOAD_USER_FAIL, payload: error.response.data.message });
+    }
+  };
+  
