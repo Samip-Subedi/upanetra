fa4e850bd3edd3bf3b73bb6dcf553a029650cf9e
Samip Subedi <samipsubedi68@gmail.com>
2025-03-02 12:07:06 +0545

    uploading get

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -167,3 +167,50 @@ export const deleteProduct = (id) => async (dispatch) => {
       });
     }
   };
+  
+// Update Product
+export const updateProduct = (id, productData) => async (dispatch) => {
+    try {
+      dispatch({ type: UPDATE_PRODUCT_REQUEST });
+  
+      const config = {
+        headers: { "Content-Type": "application/json" },
+      };
+  
+      const { data } = await axios.put(
+        `/api/v2/product/${id}`,
+        productData,
+        config
+      );
+  
+      dispatch({
+        type: UPDATE_PRODUCT_SUCCESS,
+        payload: data.success,
+      });
+    } catch (error) {
+      dispatch({
+        type: UPDATE_PRODUCT_FAIL,
+        payload: error.response.data.message,
+      });
+    }
+  };
+  
+  // Get All Reviews of a Product
+  export const getAllReviews = (id) => async (dispatch) => {
+    try {
+      dispatch({ type: ALL_REVIEW_REQUEST });
+  
+      const { data } = await axios.get(`/api/v2/reviews?id=${id}`);
+  
+      dispatch({
+        type: ALL_REVIEW_SUCCESS,
+        payload: data.reviews,
+      });
+    } catch (error) {
+      dispatch({
+        type: ALL_REVIEW_FAIL,
+        payload: error.response.data.message,
+      });
+    }
+  };
+  
