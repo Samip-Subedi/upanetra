6d3091857820ebfb3e40954367c67ece771a8ab1
Samip Subedi <samipsubedi68@gmail.com>
2025-03-02 12:10:18 +0545

    adding

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -88,4 +88,36 @@ export const loadUser = () => async (dispatch) => {
       dispatch({ type: LOAD_USER_FAIL, payload: error.response.data.message });
     }
   };
+  // Log out user
+export const logout = () => async (dispatch) => {
+    try {
+      await axios.get(`/api/v2/logout`);
+  
+      dispatch({ type: LOGOUT_SUCCESS });
+    } catch (error) {
+      dispatch({ type: LOGOUT_FAIL, payload: error.response.data.message });
+    }
+  };
+  
+  // Update Profile
+  export const updateProfile = (userData) => async (dispatch) => {
+    try {
+      dispatch({ type: UPDATE_PROFILE_REQUEST });
+  
+      const config = { headers: { "Content-Type": "multipart/form-data" } };
+  
+      const { data } = await axios.put(
+        `/api/v2/me/update/profile`,
+        userData,
+        config
+      );
+  
+      dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.success });
+    } catch (error) {
+      dispatch({
+        type: UPDATE_PROFILE_FAIL,
+        payload: error.response.data.message,
+      });
+    }
+  };
   
