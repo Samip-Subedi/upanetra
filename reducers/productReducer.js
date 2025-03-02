88813c7f149664369a8a6c7672cf8872e47fe2cb
Samip Subedi <samipsubedi68@gmail.com>
2025-03-02 12:34:25 +0545

    product added

--- reducers/productReducer.js
+++ reducers/productReducer.js
@@ -33,3 +33,72 @@ import {
     UPDATE_PRODUCT_RESET,
     UPDATE_PRODUCT_SUCCESS,
   } from "../constants/ProductConstants";
+  
+export const productreducer = (state = { products: [] }, action) => {
+    switch (action.type) {
+      case ALL_PRODUCT_REQUEST:
+      case ADMIN_PRODUCT_REQUEST:
+        return {
+          loading: true,
+          products: [],
+        };
+  
+      case ALL_PRODUCT_SUCCESS:
+        return {
+          loading: false,
+          products: action.payload.products,
+          productsCount: action.payload.productsCount,
+          resultPerPage: action.payload.resultPerPage,
+          filteredProductsCount: action.payload.filteredProductsCount,
+        };
+      case ADMIN_PRODUCT_SUCCESS:
+        return {
+          loading: false,
+          products: action.payload,
+        };
+      case ALL_PRODUCT_FAIL:
+      case ADMIN_PRODUCT_FAIL:
+        return {
+          loading: false,
+  
+          error: action.payload,
+        };
+  
+      case CLEAR_ERRORS:
+        return {
+          ...state,
+  
+          error: null,
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
+export const productDetailsReducer = (state = { product: {} }, action) => {
+    switch (action.type) {
+      case PRODUCT_DETAILS_REQUEST:
+        return {
+          loading: true,
+          ...state,
+        };
+      case PRODUCT_DETAILS_SUCCESS:
+        return {
+          loading: false,
+          product: action.payload,
+        };
+      case PRODUCT_DETAILS_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        };
+      case CLEAR_ERRORS:
+        return {
+          ...state,
+          error: null,
+        };
+      default:
+        return state;
+    }
+  };
